4203ab0c53c6470fde711e78ec2956310cfc7a28
li xingyang <176840078@qq.com>
2022-02-20 19:44:46 +0700

    整理目录

---
+++ 01-practice-2022-0220/00-import.py
@@ -0,0 +1,17 @@
+"""
+在 python 用 import 或者 from...import 来导入相应的模块。
+● 将整个模块(somemodule)导入，格式为： import somemodule
+● 从某个模块中导入某个函数,格式为： from somemodule import somefunction
+● 从某个模块中导入多个函数,格式为： from somemodule import firstfunc, secondfunc, thirdfunc
+● 将某个模块中的全部函数导入，格式为： from somemodule import *
+"""
+
+import sys # 导入整个包
+from sys import path  # 导入特定的成员
+
+
+for cur in sys.argv:
+    print('\n打印sys.argv ■:', sys.argv)
+
+print('\n打印path ■:', path) # 因为已经导入path成员，所以此处引用时不需要加sys.path
+
---
+++ 01-practice-2022-0220/数据类型/字符串.py
@@ -0,0 +1,7 @@
+'''
+Author: 李星阳
+Date: 2022-02-20 19:42:44
+LastEditors: 李星阳
+LastEditTime: 2022-02-20 19:42:45
+Description: 
+'''
